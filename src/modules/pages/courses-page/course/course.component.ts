adc91f4f1346b148e0488a676a2b7dcc955bdc3f
Aliaksandr Kassirau <aliaksandr.kassirau@gmail.com>
2020-01-12 18:57:14 +0300

    feat(courses page): module 2

--- src/modules/pages/courses-page/course/course.component.ts
+++ src/modules/pages/courses-page/course/course.component.ts
@@ -1,17 +1,60 @@
 import { ICourse } from './../icourse';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output, SimpleChanges,
+  OnChanges, OnInit,
+  AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked,
+  OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.less']
 })
-export class CourseComponent implements OnInit {
+export class CourseComponent implements OnInit, OnChanges,
+ AfterContentInit, AfterContentChecked,
+ AfterViewChecked, AfterViewInit, OnDestroy {
+
+  constructor() { }
 
   @Input()
   public course: ICourse;
 
-  constructor() { }
+  @Output()
+  public deleted = new EventEmitter<ICourse>();
+
+  @Output()
+  public edited = new EventEmitter<ICourse>();
+
+  public edit() {
+    this.edited.emit(this.course);
+  }
+
+  public delete() {
+    this.deleted.emit(this.course);
+  }
+
+  public ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges', changes);
+  }
+
+  public ngAfterContentInit() {
+    console.log('ngAfterContentInit');
+  }
+
+  public ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  public ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+  public ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  public ngOnDestroy() {
+    console.log('ngOnDestroy');
+  }
 
   public ngOnInit() {
   }
--- src/modules/pages/courses-page/courses-container/courses-container.component.ts
+++ src/modules/pages/courses-page/courses-container/courses-container.component.ts
@@ -1,23 +1,60 @@
 import { Course } from './../course';
 import { ICourse } from './../icourse';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-courses-container',
   templateUrl: './courses-container.component.html',
   styleUrls: ['./courses-container.component.less']
 })
-export class CoursesContainerComponent implements OnInit {
+export class CoursesContainerComponent implements OnInit, OnChanges {
+  private static defaultDescription = `Learn about where you can
+    find course descriptions, what information they include,
+    how they work, and details about various components
+    of a course description. Course descriptions report
+    information about a university or college\'s classes.
+    They\'re published both in course catalogs that outline
+    degree requirements and in course schedules that contain descriptions
+    for all courses offered during a particular semester.`;
 
   public courses: ICourse[];
-
+  private sequence: number;
   constructor() { }
 
   public ngOnInit() {
+    console.log('ngOnInit');
+    this.sequence = 0;
     this.courses = [
-      new Course(1, 'Video Course 1. Name tag', new Date(), 15, 'Lorem ipsum'),
-      new Course(2, 'Video Course 2. Name tag', new Date(), 15, 'Lorem ipsum')
+      this.generateCourse(),
+      this.generateCourse(),
+      this.generateCourse()
     ];
   }
 
+  public ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges:', changes);
+  }
+
+  public onCourseAdded(course: ICourse) {
+    const addedCourse: ICourse = this.generateCourse();
+    this.courses.push(addedCourse);
+    console.log('Added course: ', addedCourse);
+  }
+
+  // TODO: add interface for managing courses lifecycle
+  public onCourseEdited(editedCourse: ICourse) {
+    console.log('Edited course: ', editedCourse);
+  }
+
+  public onCourseDeleted(deletedCourse: ICourse) {
+    this.courses = this.courses.filter((course: Course) => course.id !== deletedCourse.id);
+    console.log('Deleted course: ', deletedCourse);
+  }
+
+  private generateCourse(): ICourse {
+    return new Course(++this.sequence,
+      `Video Course ${this.sequence}. Name tag`,
+      new Date(), 15,
+      CoursesContainerComponent.defaultDescription);
+  }
 }
